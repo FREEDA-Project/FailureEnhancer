ae428e8af45301c7de6e254a6bea7041df20df6b
Stefano <stefano.forti@di.unipi.it>
2024-09-16 12:02:39 +0200

    Create input.pl

---
+++ input.pl
@@ -0,0 +1,10 @@
+deployedTo(a,large,n42).
+deployedTo(b,tiny,n42).
+
+overloaded(n42,ram,1,10).
+
+unreachable(a,5).
+internal(b,3).
+internal(dummy,-1).
+disconnected(dummy, -1, -1).
+networkCongestion(dummy,dummy,-1,-1).
