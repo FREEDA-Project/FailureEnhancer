22448aabd675a2672024a17c4b8caf8411b5746d
Stefano <stefano.forti@di.unipi.it>
2024-09-16 11:37:08 +0200

    Create main.pl
    First proof-of-concept

---
+++ main.pl
@@ -0,0 +1,26 @@
+allSuggested(Constraints) :-
+    findall(Constraint, suggested(Constraint), Cs),
+    sort(Cs, Constraints). % remove duplicates
+
+suggested(affinity(d(C,FC),d(S,SF))) :-
+    deployedTo(C,FC,N), deployedTo(S,SF,M), dif(N,M),
+    timeoutEvent(C,S,_).
+    
+suggested(antiaffinity(d(C,FC),d(S,SF))) :-
+    (failure(C,_) ; failure(S,_)),
+    deployedTo(C,FC,N), deployedTo(S,SF,N).
+
+suggested(avoid(d(C,FC),N)) :-
+    deployedTo(C,FC,N),
+    anomaly(N,TI,TF), 
+    failureEvent(C,TE), between(TI,TF,TE).  % between(TI, TF, TE) <=> TI ≤ TE ≤ TF
+
+suggested(avoid(d(C,FC),N)) :-
+    deployedTo(C,FC,N), deployedTo(S,_,M), dif(N,M),
+    networkCongestion(N,M,TI,TF), % Hp: networkCongestion(N,M) -> networkCongestion(M,N)
+    timeoutEvent(C,S,TE), between(TI,TF,TE).
+    
+failure(S,T) :- unreachable(S,T); internal(S,T).
+
+anomaly(N,TI,TF) :- overloaded(N,_,TI,TF); % Resource \in {CPU,RAM,HDD,BW}
+                    disconnected(N,TI,TF). 
