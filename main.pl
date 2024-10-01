ea1ab5d29a5691147abffc13a4b5e5df7b0b6c9c
Francisco Ponce <francisco.ponce90@gmail.com>
2024-10-01 11:57:16 +0200

    last version 30-09

--- main.pl
+++ main.pl
@@ -1,29 +1,28 @@
 :- ['input.pl'].
 
 allSuggested(Constraints) :-
-    findall(Constraint, suggested(Constraint), Cs),
-    sort(Cs, Constraints).
+    findall(Constraint, distinct(suggested(Constraint)), Constraints).
 
 suggested(affinity(d(C,FC),d(S,FS))) :-
     deployedTo(C,FC,N), deployedTo(S,FS,M), dif(C,S), dif(N,M), 
     timeoutEvent(C,S,T),
-    \+( congested(N,M,T); disconnected(M,T) ).
+    \+( congested(N,M,T); disconnected(N,T) ).
     
 suggested(antiaffinity(d(C,FC),d(S,FS))) :-
     deployedTo(C,FC,N), componentFailure(C,T),
     overloaded(N,R,T),
-    race(R,C,FC,S,FS,T).
+    race(N,R,C,FC,S,FS,T).
 
 suggested(avoid(d(C,FC),N)) :-
     deployedTo(C,FC,N),
     componentFailure(C,T), 
     ( disconnected(N,T); overloaded(N,_,T) ),
-    \+ race(_,C,FC,_,_,T).
+    \+ race(N,_,C,FC,_,_,T).
 
 suggested(avoid(d(C,FC),N)) :-
     deployedTo(C,FC,N), deployedTo(S,_,M), dif(C,S), dif(N,M),
     timeoutEvent(C,S,T),
-    congested(N,M,T).
+    ( congested(N,M,T); disconnected(N,T) ).
    
 componentFailure(S,T) :- unreachable(S,T); internal(S,T).
 
@@ -33,7 +32,7 @@ congested(N,M,T) :- congestion(M,N,TI,TF), dif(N,M), between(TI,TF,T).
 disconnected(N,T) :- disconnection(N,TI,TF), between(TI,TF,T).
 overloaded(N,R,T) :- overload(N,R,TI,TF), between(TI,TF,T).
 
-race(R,C,FC,S,FS,T) :-
+race(N,R,C,FC,S,FS,T) :-
     resourceIntensive(C,FC,R,N,T),
     resourceIntensive(S,FS,R,N,T), dif(S,C).
 
